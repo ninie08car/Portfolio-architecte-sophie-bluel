40a6dd838a20a1894e83e2906495a589e7408f84
Annie Berjou <annie.carrau@wanadoo.fr>
2025-11-30 18:48:38 +0100

    "Ajout du contenu dynamique"

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,49 +1,28 @@
 // Récupération des pièces depuis le fichier JSON
-const reponse = await fetch("pieces-autos.json");
-const pieces = await reponse.json();
+const reponse = await fetch("works.json");
+const works = await reponse.json();
 
-function genererPieces(pieces) {
-  for (let i = 0; i < pieces.length; i++) {
-    const article = pieces[i];
-    // Récupération de l'élément du DOM qui accueillera les fiches
-    const sectionFiches = document.querySelector(".fiches");
+function genererWorks(works) {
+  // Récupération de l'élément du DOM qui accueillera les gallery
+  const sectionGallery = document.querySelector(".gallery");
+  for (let i = 0; i < works.length; i++) {
+    const article = works[i];
     // Création d’une balise dédiée à une pièce automobile
-    const pieceElement = document.createElement("article");
+    const worksElement = document.createElement("figure");
     // Création des balises
     const imageElement = document.createElement("img");
-    imageElement.src = article.image;
-    const nomElement = document.createElement("h2");
-    nomElement.innerText = article.nom;
-    const prixElement = document.createElement("p");
-    prixElement.innerText = `Prix: ${article.prix} € (${
-      article.prix < 35 ? "€" : "€€€"
-    })`;
-    const categorieElement = document.createElement("p");
-    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-    const descriptionElement = document.createElement("p");
-    descriptionElement.innerText =
-      article.description ?? "Pas de description pour le moment.";
-    const stockElement = document.createElement("p");
-    stockElement.innerText = article.disponibilite
-      ? "En stock"
-      : "Rupture de stock";
+    imageElement.src = article.imageUrl;
+    imageElement.alt = article.title;
+    const nomElement = document.createElement("figcaption");
+    nomElement.innerText = article.title;
 
-    //Code ajouté
-    const avisBouton = document.createElement("button");
-    avisBouton.dataset.id = article.id;
-    avisBouton.textContent = "Afficher les avis";
+    // On rattache la balise article a la section Gallery
+    sectionGallery.appendChild(worksElement);
 
-    // On rattache la balise article a la section Fiches
-    sectionFiches.appendChild(pieceElement);
     // On rattache l’image à pieceElement (la balise article)
-    pieceElement.appendChild(imageElement);
-    pieceElement.appendChild(nomElement);
-    pieceElement.appendChild(prixElement);
-    pieceElement.appendChild(categorieElement);
-    //Ajout des éléments au DOM pour l'exercice
-    pieceElement.appendChild(descriptionElement);
-    pieceElement.appendChild(stockElement);
+    worksElement.appendChild(imageElement);
+    worksElement.appendChild(nomElement);
   }
 }
 
-genererPieces(pieces);
+genererWorks(works);
