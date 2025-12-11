00ad5a9b084a49a047f01683c4fce07117a7d4dd
Annie Berjou <annie.carrau@wanadoo.fr>
2025-12-11 17:14:25 +0100

    mise à jour code pour login

--- FrontEnd/JS/api.js
+++ FrontEnd/JS/api.js
@@ -19,23 +19,14 @@ async function getCategories() {
 }
 
 async function postLogin(email, password) {
-  try {
-    const response = await fetch(apiUrl + "/user/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    });
-
-    const data = await response.json();
-
-    if (!response.ok) {
-      throw new Error(data.message || "Erreur de connexion");
-    }
-
-    return data;
-  } catch (error) {
-    throw new Error(error.message || "Impossible de contacter le serveur");
-  }
+  fetch(apiUrl + "/users/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  });
 }
 
 async function postWorks(image, title, category) {
